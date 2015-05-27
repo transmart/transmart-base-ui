1908b54d96d8fe0912944c6268db19ce4b788460
jeremy <jeremy@thehyve.nl>
2015-05-27 10:22:18 +0200

    integrate to study

--- src/app/main/sidebar/sidebar.controller.js
+++ src/app/main/sidebar/sidebar.controller.js
@@ -4,53 +4,8 @@ angular.module('transmartBaseUi')
   .controller('SidebarCtrl',
   ['$scope', 'Restangular', 'dataService', function ($scope, Restangular, dataService) {
 
-    $scope.data = [];
     $scope.studies = [];
 
-    $scope.remove = function(scope) {
-      scope.remove();
-    };
-
-    $scope.toggle = function(scope) {
-      scope.toggle();
-    };
-
-    $scope.moveLastToTheBegginig = function () {
-      var a = $scope.data.pop();
-      $scope.data.splice(0,0, a);
-    };
-
-    $scope.newSubItem = function(scope) {
-      var nodeData = scope.$modelValue;
-      nodeData.nodes.push({
-        id: nodeData.id * 10 + nodeData.nodes.length,
-        title: nodeData.title + '.' + (nodeData.nodes.length + 1),
-        nodes: []
-      });
-    };
-
-    var getRootNodesScope = function() {
-      return angular.element(document.getElementById("tree-root")).scope();
-    };
-
-    $scope.collapseAll = function() {
-      var scope = getRootNodesScope();
-      scope.collapseAll();
-    };
-
-    $scope.expandAll = function() {
-      var scope = getRootNodesScope();
-      scope.expandAll();
-    };
-
-    $scope.status = {
-      isFirstOpen: true,
-      isFirstDisabled: false
-    };
-
-    $scope.oneAtATime = true;
-
-    $scope.data = dataService.studies.getTree();
     $scope.studies = dataService.studies.getStudies();
 
   }]);
