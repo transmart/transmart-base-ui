dc59f472c608d80d0f767d4619c025786a6932e1
Riza Nugraha <rnugraha@gmail.com>
2015-06-01 01:42:47 +0200

    Invoke display chart when user clicking a node

--- src/app/main/sidebar/chart.service.js
+++ src/app/main/sidebar/chart.service.js
@@ -11,16 +11,15 @@ angular.module('transmartBaseUi')
      * @param node
      */
     chartService.displayStats = function (node, study) {
-      console.log('node', node);
-      console.log('study', study._links.self.href);
 
       var _path = node.link.slice(1);
-      console.log( _path);
 
       Restangular.all(_path + '/observations').getList()
         .then(function (d) {
 
-          console.log(d);
+          console.log(d.length);
+
+
         }, function () {
 
         });
