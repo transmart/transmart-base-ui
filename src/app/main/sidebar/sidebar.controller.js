b05a8e05de58086d5f5757ba47d24c9bed46a98a
jgeofil <jeremy@thehyve.nl>
2015-06-03 16:11:28 +0200

    Refactored sidebar.controller

--- src/app/main/sidebar/sidebar.controller.js
+++ src/app/main/sidebar/sidebar.controller.js
@@ -1,10 +1,11 @@
 'use strict';
 
 angular.module('transmartBaseUi')
-  .controller('SidebarCtrl',
-
-  ['$scope', '$window', 'Restangular', 'endpointService',
-  function ($scope, $window, Restangular, endpointService) {
+  .controller('SidebarCtrl', ['$scope', '$window', 'Restangular', 'endpointService',
+    function ($scope, $window, Restangular, endpointService) {
+    //------------------------------------------------------------------------------------------------------------------
+    // Scope
+    //------------------------------------------------------------------------------------------------------------------
 
     $scope.endpoints = [];
     $scope.formData = {};
@@ -12,8 +13,42 @@ angular.module('transmartBaseUi')
     $scope.publicStudies = [];
     $scope.privateStudies = [];
 
+    $scope.addResource = function() {
+      var formData = $scope.formData;
+      if (formData.requestToken) {
+        endpointService.addOAuthEndpoint(formData.title, formData.url, formData.requestToken)
+          .then(function() {
+            resetEndpointForm();
+            loadStudies();
+          });
+      }
+      else {
+        endpointService.addEndpoint(formData.title, formData.url);
+        resetEndpointForm();
+        loadStudies();
+      }
+    };
+
+    $scope.navigateToAuthorizationPage = function() {
+      var url = $scope.formData.url;
+
+      // Cut off any '/'
+      if (url.substring(url.length-1, url.length) === '/') {
+        url = url.substring(0, url.length-1);
+      }
+
+      var authorizationUrl = url + '/oauth/authorize?response_type=code&client_id=api-client&client_secret=api-client&redirect_uri=' + url + '/oauth/verify';
+      $window.open(authorizationUrl, '_blank');
+    };
+
+    //------------------------------------------------------------------------------------------------------------------
+    // Functionality
+    //------------------------------------------------------------------------------------------------------------------
     loadStudies();
 
+    //------------------------------------------------------------------------------------------------------------------
+    // Helper functions
+    //------------------------------------------------------------------------------------------------------------------
     function loadStudies() {
       var endpoints = endpointService.getEndpoints();
       $scope.endpoints = endpoints;
@@ -55,33 +90,5 @@ angular.module('transmartBaseUi')
       formData.requestToken = '';
       formData.endpointForm.$setPristine();
     }
-
-    $scope.navigateToAuthorizationPage = function() {
-      var url = $scope.formData.url;
-
-      // Cut off any '/'
-      if (url.substring(url.length-1, url.length) === '/') {
-        url = url.substring(0, url.length-1);
-      }
-
-      var authorizationUrl = url + '/oauth/authorize?response_type=code&client_id=api-client&client_secret=api-client&redirect_uri=' + url + '/oauth/verify';
-      $window.open(authorizationUrl, '_blank');
-    };
-
-    $scope.addResource = function() {
-      var formData = $scope.formData;
-      if (formData.requestToken) {
-        endpointService.addOAuthEndpoint(formData.title, formData.url, formData.requestToken)
-          .then(function() {
-            resetEndpointForm();
-            loadStudies();
-          });
-      }
-      else {
-        endpointService.addEndpoint(formData.title, formData.url);
-        resetEndpointForm();
-        loadStudies();
-      }
-    };
 
   }]);
