1fcc8b3c8548ba1c971c825ae4325dd5788d99d4
jgeofil <jeremy@thehyve.nl>
2015-06-05 12:07:36 +0200

    Added info popover to node summary tab

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -17,6 +17,12 @@ angular.module('transmartBaseUi')
       }
     };
 
+    $scope.metadata = {
+      Title: 'Node title',
+      Organism: 'Homo sapiens',
+      Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris faucibus ut nisl quis ullamcorper. Quisque in orci vitae nibh rhoncus blandit. Integer tincidunt nunc sit amet magna faucibus, eget pellentesque libero finibus. Sed eu cursus risus, ac pretium felis. In non turpis eros. Nam nec tellus venenatis, consectetur dui a, posuere dui. In id pellentesque elit, ac mattis orci. Donec aliquam feugiat neque nec efficitur. Donec fermentum posuere diam, quis semper felis aliquam vel. Praesent sit amet dapibus tortor. Aliquam sed quam non augue imperdiet scelerisque. Vivamus pretium pretium eros. Nullam finibus accumsan tempor. Duis mollis, ex nec maximus bibendum.'
+    };
+
     $scope.observations = [];
 
     $scope.getStudyConcepts = function (studyLink, studyId) {
@@ -107,6 +113,7 @@ angular.module('transmartBaseUi')
     };
 
     $scope.displayNodeSummaryStatistics = function (node) {
+      $scope.selectednode = node;
       ChartService.getObservations(node).then(function (d) {
         // at first, get the observation data for the selected node
         $scope.$apply(function () {
