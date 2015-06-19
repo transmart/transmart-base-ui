8974f96ac34d18a2640cedabb733d745ff0c43f5
jgeofil <jeremy@thehyve.nl>
2015-06-19 16:58:54 +0200

    Clean up

--- src/app/main/sidebar/study.controller.js
+++ src/app/main/sidebar/study.controller.js
@@ -23,25 +23,23 @@ angular.module('transmartBaseUi')
       oneAtATime: true,
     };
 
-
-    $scope.type = {
-      fol: function(node){return node.type === 'FOLDER';},
-      num: function(node){return node.type === 'NUMERICAL';},
-      cat: function(node){return node.type === 'CATEGORICAL';},
-      hid: function(node){return node.type === 'HIGH_DIMENSIONAL';}
-    };
-
+    /**
+     * When a study is selected, get the tree
+     * @param study
+     */
     $scope.getTree = function (study) {
       $scope.selectedStudy = study;
       if(!$scope.opened){
-        $scope.treeLoading = true;
         $scope.tree = _getSingleTree(study);
         $scope.opened = true;
       }
     };
 
-    $scope.populateChilds = function (node)
-    {
+    /**
+     * When a node is clicked, get children of the children
+     * @param node
+     */
+    $scope.populateChilds = function (node) {
       node.nodes.forEach(function(child){
         _getNodeChildren(child, false, '');
       })
@@ -72,6 +70,11 @@ angular.module('transmartBaseUi')
       }
     });
 
+    /**
+     * Counts the subjects for a node
+     * @param node
+     * @private
+     */
     var _countSubjects = function(node) {
       if(!node.hasOwnProperty('total')){
         node.restObj.one('subjects').get().then(function(subjects){
@@ -80,27 +83,13 @@ angular.module('transmartBaseUi')
       }
     };
 
-    var orderTreeNodes = function (tree) {
-      // Order the nodes by type with folders first
-      var folders = [];
-      var numerical = [];
-      var catego = [];
-      var high = [];
-
-      if(tree.hasOwnProperty('nodes')){
-        tree.nodes.forEach(function (node){
-          if(node.type === 'FOLDER') {folders.push(node);}
-          if(node.type === 'NUMERICAL') {numerical.push(node);}
-          if(node.type === 'CATEGORICAL') {catego.push(node);}
-          if(node.type === 'HIGH_DIMENSIONAL') {high.push(node);}
-        });
-        tree.nodes = folders.concat(numerical).concat(catego).concat(high);
-
-        // Traverse the tree
-        tree.nodes.forEach(orderTreeNodes);
-      }
-    };
-
+    /**
+     * Populates 2 levels of children for the node
+     * @param node
+     * @param end IF TRUE runs only for one level
+     * @param prefix
+     * @private
+     */
     var _getNodeChildren = function(node, end, prefix){
       prefix = prefix || '';
       var children = node.restObj._links.children;
@@ -135,6 +124,12 @@ angular.module('transmartBaseUi')
       if(!end) node.loaded = true;
     };
 
+    /**
+     * Populates the first 2 levels of a study tree
+     * @param study
+     * @returns {{title: string, nodes: Array, restObj: *, loaded: boolean}}
+     * @private
+     */
     var _getSingleTree = function(study) {
       study._links.children = study._embedded.ontologyTerm._links.children;
       var tree = {
@@ -148,3 +143,34 @@ angular.module('transmartBaseUi')
     };
 
   }]);
+
+/**
+ 
+ $scope.type = {
+      fol: function(node){return node.type === 'FOLDER';},
+      num: function(node){return node.type === 'NUMERICAL';},
+      cat: function(node){return node.type === 'CATEGORICAL';},
+      hid: function(node){return node.type === 'HIGH_DIMENSIONAL';}
+    };
+
+ var orderTreeNodes = function (tree) {
+  // Order the nodes by type with folders first
+  var folders = [];
+  var numerical = [];
+  var catego = [];
+  var high = [];
+
+  if(tree.hasOwnProperty('nodes')){
+    tree.nodes.forEach(function (node){
+      if(node.type === 'FOLDER') {folders.push(node);}
+      if(node.type === 'NUMERICAL') {numerical.push(node);}
+      if(node.type === 'CATEGORICAL') {catego.push(node);}
+      if(node.type === 'HIGH_DIMENSIONAL') {high.push(node);}
+    });
+    tree.nodes = folders.concat(numerical).concat(catego).concat(high);
+
+    // Traverse the tree
+    tree.nodes.forEach(orderTreeNodes);
+  }
+};
+ **/
