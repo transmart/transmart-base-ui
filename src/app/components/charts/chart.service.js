61b8e551eb6cc369bc998d7213d03ff06463e9ed
jgeofil <jeremy@thehyve.nl>
2015-06-25 14:31:12 +0200

    Commit without proxy.js

--- src/app/components/charts/chart.service.js
+++ src/app/components/charts/chart.service.js
@@ -4,27 +4,7 @@ angular.module('transmartBaseUi')
 
     .factory('ChartService', ['Restangular', '$q', '$rootScope', '$timeout',
         function (Restangular, $q, $rootScope, $timeout) {
-
             var chartService = {};
-
-            /**
-             * Chart data constructor
-             * @param idx
-             * @param label
-             * @param title
-             * @param dataType
-             * @param observations
-             * @returns {{id: *, label: *, title: *, type: *, observations: *}}
-             */
-            var newChartData = function (idx, label, title, dataType, observations) {
-                return {
-                    id: idx,
-                    label: label,
-                    title: title,
-                    type: dataType,
-                    observations: observations
-                };
-            };
 
             /**
              * Create dc.js bar chart
@@ -33,15 +13,17 @@ angular.module('transmartBaseUi')
              * @param el
              * @private
              */
-            var _barChart = function (cDimension, cGroup, el, min, max, nodeTitle, width) {
+            var _barChart = function (cDimension, cGroup, el, min, max, nodeTitle, width, height, btmMarg) {
 
                 width = width || 270;
+                height = height || 210;
+                btmMarg = btmMarg || 30;
 
                 var _barChart = dc.barChart(el);
                 _barChart
                     .width(width)
-                    .height(200)
-                    .margins({top: 5, right: 5, bottom: 30, left: 25})
+                    .height(height)
+                    .margins({top: 5, right: 5, bottom: btmMarg, left: 25})
                     .dimension(cDimension)
                     .group(cGroup)
                     .elasticY(true)
@@ -69,36 +51,25 @@ angular.module('transmartBaseUi')
              * @returns {*}
              * @private
              */
-            var _pieChart = function (cDimension, cGroup, el) {
+            var _pieChart = function (cDimension, cGroup, el, size, nolegend) {
                 var tChart = dc.pieChart(el);
 
+                nolegend = nolegend || false;
+                size = size || 200;
+
                 tChart
-                    .width(270)
-                    .height(200)
+                    .width(size)
+                    .height(size)
                     .innerRadius(0)
                     .dimension(cDimension)
                     .group(cGroup)
-                    .renderLabel(false)
-                    .legend(dc.legend());
+                    .renderLabel(false);
+
+                if(!nolegend){
+                    tChart.legend(dc.legend());
+                }
 
                 return tChart;
-            };
-
-            /**
-             * To find element in array based on object's key:value
-             * @param arr
-             * @param propName
-             * @param propValue
-             * @returns {*}
-             * @private
-             */
-            var _findElement = function (arr, propName, propValue) {
-                for (var i = 0; i < arr.length; i++) {
-                    if (arr[i][propName] === propValue) {
-                        return arr[i];
-                    }
-                }
-                // will return undefined if not found; you could return a default instead
             };
 
             /**
@@ -113,149 +84,51 @@ angular.module('transmartBaseUi')
             };
 
             /**
-             * get data type
-             * @param val
-             * @returns {string}
-             * @private
-             */
-            var _getDataType = function (val) {
-                var _type = typeof val;
-                if (_type === 'string' || _type === 'number') {
-                    return _type;
-                }
-                return 'unknown';
-            };
-
-            /**
-             * Group observations based on its labels
-             * @param d
-             * @returns {Array}
-             * @private
-             */
-            var _createGroupBasedOnObservationsLabel = function (d) {
-                var _d = [];
-
-                d.forEach(function (o, idx) {
-                    var _x = _findElement(_d, 'label', o.label);
-                    //var _dataType = _getDataType(o.value);
-                    //console.log(_dataType);
-                    if (typeof _x === 'undefined') {
-                        _d.push(newChartData(idx, o.label, _getLastToken(o.label), _getDataType(o.value), [{value: o.value}]));
-                    } else {
-                        _x.observations.push({value: o.value});
-                    }
-                });
-
-                return _d;
-            };
-
-
-            var _createGroupBasedOnSubjectAttributes = function (d) {
-                var _d = [],
-                    _keys = ['trial', 'inTrialId', 'birthDate', 'deathDate', 'id'],
-                    _subjects = d._embedded.subjects;
-
-                if (_subjects.length > 0) {
-
-                    angular.forEach(_subjects, function (subject) { // iterate through subjects
-                        var _idx = 0;
-                        angular.forEach(subject, function (value, key) { // iterate through subject properties
-
-                            var _x = _findElement(_d, 'label', key); // check if label is already existing
-                            var _dataType = _getDataType(subject[key]);
-
-                            // only for known data types and keys
-                            if (_dataType !== 'unknown' && (_keys.indexOf(key) === -1)) {
-                                if (typeof _x === 'undefined') { // create new data chart when key is not yet in the collection
-                                    _d.push(newChartData(_idx, key, key, _dataType, [{value: subject[key]}]));
-                                    _idx++;
-                                } else { // otherwise add the data to the existing key
-                                    _x.observations.push({value: subject[key]});
-                                }
-                            }
-
-                        }); //end forEach subject properties
-                    }); //end forEach subjects
-
-                }
-                return _d;
-            };
-
-            chartService.getObservations = function (node) {
-                var _observationsList = [];
-
-                var promise = new Promise(function (resolve, reject) {
-                    node.restObj.one('observations').get()
-                        .then(function (d) {
-                            d = d._embedded.observations;
-                            // create categorical or numerical dimension based on observation data
-                            _observationsList = _createGroupBasedOnObservationsLabel(d);
-                            resolve(_observationsList);
-                        }, function (err) {
-                            reject('Cannot get data from the end-point.' + err);
-                        });
-                });
-
-                return promise;
-            };
-
-            chartService.getSubjects = function (study) {
-                var selectedStudy = {};
-
-                var promise = new Promise(function (resolve, reject) {
-                    study.one('subjects').get()
-                        .then(function (d) {
-                            selectedStudy.subjects = d._embedded.subjects;
-                            selectedStudy.chartData = _createGroupBasedOnSubjectAttributes(d);
-                            resolve(selectedStudy);
-                        }, function (err) {
-                            reject('Cannot get subjects from the end-point.' + err);
-                        });
-                }); //end Promise
-
-                return promise;
-            };
-
-            chartService.generateCharts = function (nodes) {
-                var _charts = [], _deferred = $q.defer(), idx = 0;
-
-                angular.forEach(nodes, function (node) {
-                    var ndx = crossfilter(node.observations),
-                        tDimension = ndx.dimension(function (d) {
-                            return d.value;
-                        }),
-                        tGroup = tDimension.group();
-
-                    if (node.type === 'string') {
-                        _charts.push(_pieChart(tDimension, tGroup, '#chart_' + idx));
-                    } else if (node.type === 'number') {
-                        var _max = Math.max.apply(Math, node.observations.map(function (o) {
-                                return o.value;
-                            })),
-                            _min = Math.min.apply(Math, node.observations.map(function (o) {
-                                return o.value;
-                            }));
-                        _charts.push(_barChart(tDimension, tGroup, '#chart_' + idx, _min, _max, node.title));
-                    }
-
-                    idx++;
-
-                    if (idx === nodes.length) {
-                        _deferred.resolve(_charts);
-                    }
-                });
-
-                return _deferred.promise;
-
-            };
-
+            *
+            * @param charts
+            */
             chartService.renderAll = function (charts) {
                 angular.forEach(charts, function (chart) {
-                    chart.render();
+                  chart.render();
                 });
             };
 
-            /*******************************************************************************************************************
+            /***********************************************************************************************************
+            * Summary statistics chart service
+            */
+            var ss = {};
+
+            chartService.displaySummaryStatistics = function(study, magicConcepts){
+                study.one('subjects').get().then(function (d) {
+                    var sub = d._embedded.subjects;
+
+                    ss = {
+                        charts: [],
+                        cross: crossfilter(sub),
+                        dims: {},
+                        grps: {},
+                    };
+
+                    magicConcepts.forEach(function(concept){
+                        ss.dims[concept] = ss.cross.dimension(function(d){return d[concept];})
+                        ss.grps[concept] = ss.dims[concept].group();
+
+                        if (typeof sub[0][concept] === 'string' || typeof sub[0][concept] === 'object') {
+                          ss.charts.push(_pieChart(ss.dims[concept], ss.grps[concept], '#summary-chart-' + concept, 75, true));
+                        } else if (typeof sub[0][concept] === 'number') {
+                          var max = ss.dims[concept].top(1)[0][concept];
+                          var min = ss.dims[concept].bottom(1)[0][concept];
+                          ss.charts.push(_barChart(ss.dims[concept], ss.grps[concept], '#summary-chart-' + concept, min-5, max+5, '', 600, 100, 5));
+                        }
+                    });
+                    chartService.renderAll(ss.charts);
+
+                }, function (err) {
+                    reject('Cannot get subjects from the end-point.' + err);
+                });
+            };
+
+            /***********************************************************************************************************
              * Cohort chart service
              */
             var cs = {};
@@ -264,17 +137,17 @@ angular.module('transmartBaseUi')
              * Reset the cohort chart service to initial state
              */
             chartService.reset = function () {
-               cs = {
-                   subjects: [],
-                   chartId: 0,
-                   charts: [],
-                   cross: crossfilter(),
-                   dims: {},
-                   grps: {},
-                   labels: []
+                cs = {
+                     subjects: [],
+                     chartId: 0,
+                     charts: [],
+                     cross: crossfilter(),
+                     dims: {},
+                     grps: {},
+                     labels: []
                 };
 
-               cs.mainDim = cs.cross.dimension(function (d) {
+                cs.mainDim = cs.cross.dimension(function (d) {
                     return d.labels;
                 });
 
