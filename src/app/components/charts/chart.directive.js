f3ae084f4981a24aa9c3aa304611568ad54d94ba
jgeofil <jeremy@thehyve.nl>
2015-06-26 14:37:16 +0200

    fix directives paths

--- src/app/components/charts/chart.directive.js
+++ src/app/components/charts/chart.directive.js
@@ -3,12 +3,12 @@
 angular.module('transmartBaseUi')
   .directive('tsChart', function() {
     return {
-      templateUrl: '/app/components/charts/chart-template.html'
+      templateUrl: 'app/components/charts/chart-template.html'
     };
   })
   .directive('tsCohortChart', function() {
     return {
-      templateUrl: '/app/components/charts/cohort-chart-template.html'
+      templateUrl: 'app/components/charts/cohort-chart-template.html'
     };
   });
 
