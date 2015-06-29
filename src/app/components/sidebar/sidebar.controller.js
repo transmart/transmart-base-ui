77f111ab5b8dc7ef2587b962190fe3c41180bb69
jgeofil <jeremy@thehyve.nl>
2015-06-29 19:04:47 +0200

    Update sidebar.controller.js

--- src/app/components/sidebar/sidebar.controller.js
+++ src/app/components/sidebar/sidebar.controller.js
@@ -88,7 +88,6 @@ angular.module('transmartBaseUi')
         }
 
     $scope.populateDefaultApi = function(name, link) {
-      console.log(name)
       $scope.formData.title = name;
       $scope.formData.url = link;
       $scope.formData.requestToken = '';
