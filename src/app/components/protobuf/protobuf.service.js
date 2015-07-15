9b963ffb1b62db219a4fafc24afc3b8288e04104
jgeofil <jeremy@thehyve.nl>
2015-07-15 18:27:39 +0200

    Protobuf work

--- src/app/components/protobuf/protobuf.service.js
+++ src/app/components/protobuf/protobuf.service.js
@@ -3,8 +3,14 @@
 angular.module('transmartBaseUi').factory('ProtobufService', [function(){
   var service = {};
 
-  //Load proto file 
+  //Load proto file
   var _builder = dcodeIO.ProtoBuf.loadProtoFile("/app/components/protobuf/highdim.proto");
+  _builder = _builder.build('highdim');
+  console.log(_builder);
+
+  service.decode = function (buffer) {
+    return _builder.Row.decodeDelimited(buffer, 'hex');
+  };
 
   return service;
 }]);
--- src/app/main/high.controller.js
+++ src/app/main/high.controller.js
@@ -7,5 +7,17 @@ angular.module('transmartBaseUi').controller('HighCtrl',
 
     $scope.onNodeDropEvent = function (event, info, node) {
       $scope.concept = node;
+
+      node.restObj.one('highdim').get().then(function (metadata){
+        $scope.restObj = metadata;
+        $scope.metadata = metadata._embedded.dataTypes;
+      });
     };
+
+    $scope.getData = function (name) {
+      $scope.restObj.one('?dataType='+name).get({}, {'Accept': 'application/octet-stream'}).then(function (data){
+        var message = ProtobufService.decode(data);
+        console.log(message);
+      });
+    }
   }]);
