a9363f4d83b904b39f43576363207afa7b5b15db
Riza Nugraha <rnugraha@gmail.com>
2015-07-21 11:13:03 +0200

    remove success alert and reformat code

--- src/app/components/sidebar/sidebar.controller.js
+++ src/app/components/sidebar/sidebar.controller.js
@@ -4,51 +4,55 @@ angular.module('transmartBaseUi')
   .controller('SidebarCtrl', ['$scope', '$rootScope', '$window', 'Restangular', 'EndpointService', 'AlertService',
     function ($scope, $rootScope, $window, Restangular, EndpointService, AlertService) {
 
-    $scope.publicStudies = [];
-    $scope.privateStudies = [];
-    $scope.searchTerm = '';
+      $scope.publicStudies = [];
+      $scope.privateStudies = [];
+      $scope.searchTerm = '';
 
-    var _loadStudies = function () {
-        var endpoints = EndpointService.getEndpoints();
+      var _loadStudies = function () {
 
-        $scope.endpoints = endpoints;
+        var _endpoints = EndpointService.getEndpoints();
+
         $scope.publicStudies = [];
         $scope.privateStudies = [];
 
         // Load studies for each endpoint
-        endpoints.forEach(function(endpoint) {
-        endpoint.restangular.all('studies').getList()
+        _endpoints.forEach(function (endpoint) {
+          endpoint.restangular.all('studies').getList()
             .then(function (studies) {
 
-            // Alert user that it successfully connects to the rest-api
-            AlertService.add('success', 'Loaded studies from: ' + endpoint.url, 3000);
-            endpoint.status = 'success';
-            $scope.studies = studies;
+              // Alert user that it successfully connects to the rest-api
+              // AlertService.add('success', 'Loaded studies from: ' + endpoint.url, 3000);
 
-            // Tell homepage that studies is loaded
-            $scope.$emit('howManyStudiesLoaded', $scope.studies.length);
+              endpoint.status = 'success';
+              $scope.studies = studies;
 
-            if(studies.length > 0) {$scope.endpointTabOpen = true;}
+              // Tell homepage that studies is loaded
+              $scope.$emit('howManyStudiesLoaded', $scope.studies.length);
 
-            // Checking if studies are public or private
-            $scope.studies.forEach(function(study){
-              study.endpoint = endpoint; // Keep reference to endpoint
-              if (study._embedded.ontologyTerm.fullName.split('\\')[1] ===
-                'Public Studies') {
-                $scope.publicStudies.push(study);
-              } else {
-                $scope.privateStudies.push(study);
+              if (studies.length > 0) {
+                $scope.endpointTabOpen = true;
               }
-            });
-          }, function () {
-            AlertService.add('danger', 'Could not load studies from API: ' +
-              endpoint.url, 3000);
+
+              // Checking if studies are public or private
+              $scope.studies.forEach(function (study) {
+                study.endpoint = endpoint; // Keep reference to endpoint
+                if (study._embedded.ontologyTerm.fullName.split('\\')[1] ===
+                  'Public Studies') {
+                  $scope.publicStudies.push(study);
+                } else {
+                  $scope.privateStudies.push(study);
+                }
               });
-            endpoint.status = 'error';
-          });
-    };
+            }, function () {
+              AlertService.add('danger', 'Could not load studies from API: ' +
+                endpoint.url, 3000);
+            });
+          endpoint.status = 'error';
+        });
+      };
 
-    EndpointService.registerNewEndpointEvent(_loadStudies);
+      EndpointService.registerNewEndpointEvent(_loadStudies);
 
-    _loadStudies();
-  }]);
+      _loadStudies();
+
+    }]);
