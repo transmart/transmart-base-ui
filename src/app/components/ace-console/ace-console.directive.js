ab15e65983a61a3abe6c613e756fca311ca862e1
jgeofil <jeremy@thehyve.nl>
2015-07-24 12:44:15 +0200

    Funcitonal R console

--- src/app/components/ace-console/ace-console.directive.js
+++ src/app/components/ace-console/ace-console.directive.js
@@ -7,13 +7,39 @@ angular.module('transmartBaseUi')
       templateUrl: 'app/components/ace-console/ace-console.tpl.html',
       scope: {},
       controller: function($scope) {
-        $scope.aceOptions = {
+
+        $scope.aceOptionsIn = {
           theme: 'idle_fingers',
           mode: 'r',
           useWrapMode : true
         };
-        $scope.example = 'require(rCharts)\nrPlot(mpg ~ wt, data = mtcars,type = "point")';
 
+        $scope.aceOptionsOut = {
+          theme: 'pastel_on_dark',
+          mode: 'json',
+          useWrapMode : true
+        };
+
+        $scope.inputConsole = 'x <- 5';
+        $scope.outputConsole = '';
+
+        ocpu.seturl("//transmart-gb-opencpu.thehyve.net/ocpu/github/thehyve/rtransmartbaseui/R");
+
+        $scope.runScript = function () {
+          var req = ocpu.rpc("execscript",{
+            text : $scope.inputConsole
+          }, function(output){
+            $scope.outputConsole = JSON.stringify(output, null, '\t');
+            $scope.aceOptionsOut.mode = 'json';
+            $scope.$apply();
+          });
+
+          req.fail(function(){
+            $scope.outputConsole = req.responseText;
+            $scope.aceOptionsOut.mode = 'r';
+            $scope.$apply();
+          });
+        };
 
       }
     };
