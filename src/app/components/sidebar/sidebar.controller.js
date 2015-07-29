4d261c1599ce354811a0fc9cd787f8744c1f871b
Riza Nugraha <rnugraha@gmail.com>
2015-07-29 16:59:45 +0200

    changes do to studyservice refactoring

--- src/app/components/sidebar/sidebar.controller.js
+++ src/app/components/sidebar/sidebar.controller.js
@@ -3,15 +3,15 @@
 angular.module('transmartBaseUi')
   .controller('SidebarCtrl', ['$scope', 'StudyListService', function ($scope, StudyListService) {
 
-      $scope.publicStudies = StudyListService.public;
-      $scope.privateStudies =  StudyListService.private;
+      $scope.publicStudies = StudyListService.getPublicStudies();
+      $scope.privateStudies =  StudyListService.getPrivateStudies();
 
       $scope.searchTerm = '';
 
       var _loadStudies = function () {
         StudyListService.loadStudies().then(function () {
-          $scope.publicStudies = StudyListService.public;
-          $scope.privateStudies =  StudyListService.private;
+          $scope.publicStudies = StudyListService.getPublicStudies();
+          //$scope.privateStudies =  StudyListService.getPrivateStudies();
         });
       };
 
