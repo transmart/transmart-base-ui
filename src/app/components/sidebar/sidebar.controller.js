99f886b6a0874071edaa85198c1c2e349741823e
Riza Nugraha <rnugraha@gmail.com>
2015-07-31 17:59:15 +0200

    Enable user to sync study list

--- src/app/components/sidebar/sidebar.controller.js
+++ src/app/components/sidebar/sidebar.controller.js
@@ -2,24 +2,21 @@
 
 angular.module('transmartBaseUi')
   .controller('SidebarCtrl', ['$scope', 'StudyListService', function ($scope, StudyListService) {
-      //
+
       $scope.publicStudies = StudyListService.getPublicStudies();
       $scope.privateStudies =  StudyListService.getPrivateStudies();
-      //
 
-      $scope.searchTerm = '';
 
-      var _loadStudies = function () {
-        //console.log(ásd);
+      $scope.searchTerm =   '';
+
+      $scope.loadStudies = function () {
+        console.log('hello ..')
         StudyListService.loadStudies().then(function () {
-
           $scope.publicStudies = StudyListService.getPublicStudies();
           $scope.privateStudies =  StudyListService.getPrivateStudies();
-
+          $scope.$apply();
         });
-
       };
 
-      _loadStudies();
 
     }]);
