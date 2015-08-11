fbb5c7de73407deca19a0b56dc306368c27b9b9a
Riza Nugraha <rnugraha@gmail.com>
2015-08-11 17:29:22 +0200

    dev export/import cohort selections

--- src/app/components/import-workspace/import-workspace.controller.js
+++ src/app/components/import-workspace/import-workspace.controller.js
@@ -1,6 +1,21 @@
 'use strict';
 
 angular.module('transmartBaseUi')
-  .controller('ImportWorkspaceCtrl', ['$scope', function ($scope) {
+  .controller('ImportWorkspaceCtrl', ['$scope', '$modalInstance', function ($scope, $modalInstance) {
 
+    $scope.showContent = function($fileContent){
+      $scope.content = $fileContent;
+    };
+
+    $scope.ok = function () {
+
+      $modalInstance.close();
+
+      console.log(JSON.parse($scope.content));
+
+    };
+
+    $scope.cancel = function () {
+      $modalInstance.dismiss('cancel');
+    };
   }]);
