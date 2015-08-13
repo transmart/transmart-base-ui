938804435e2e98bb74632f821878d5bf623b2134
Riza Nugraha <rnugraha@gmail.com>
2015-08-13 12:33:26 +0200

    some unit testings

--- src/app/components/import-workspace/import-workspace.controller.js
+++ src/app/components/import-workspace/import-workspace.controller.js
@@ -3,19 +3,16 @@
 angular.module('transmartBaseUi')
   .controller('ImportWorkspaceCtrl', ['$scope', '$modalInstance', function ($scope, $modalInstance) {
 
-    $scope.showContent = function($fileContent){
+    $scope.showContent = function ($fileContent){
       $scope.content = $fileContent;
     };
 
     $scope.ok = function () {
-
       $modalInstance.close();
-
-      console.log(JSON.parse($scope.content));
-
     };
 
     $scope.cancel = function () {
       $modalInstance.dismiss('cancel');
     };
-  }]);
+
+}]);
