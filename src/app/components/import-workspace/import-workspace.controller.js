af1e66729dcfbf89ac5f53376bec270b177088b4
Riza Nugraha <rnugraha@gmail.com>
2015-08-19 10:35:05 +0200

    check if saved endpoint od the node is still active

--- src/app/components/import-workspace/import-workspace.controller.js
+++ src/app/components/import-workspace/import-workspace.controller.js
@@ -1,7 +1,8 @@
 'use strict';
 
 angular.module('transmartBaseUi')
-  .controller('ImportWorkspaceCtrl', ['$scope', '$modalInstance', 'ChartService', function ($scope, $modalInstance, ChartService) {
+  .controller('ImportWorkspaceCtrl', ['$scope', '$modalInstance', 'ChartService', 'EndpointService',
+    function ($scope, $modalInstance, ChartService, EndpointService) {
 
     $scope.readContent = function ($fileContent){
       $scope.content = JSON.parse($fileContent);
@@ -12,7 +13,28 @@ angular.module('transmartBaseUi')
 
       // TODO add nodes via chart service
       _.each($scope.content.nodes, function (node) {
-        ChartService.addNodeToActiveCohortSelection(node);
+
+        // check if endpoints are connected
+        var _e = _.where(EndpointService.endpoints, {url:node.endpoint.url});
+
+        if (_e) { // get restObj for each nodes
+          console.log(_e[0].restangular);
+          var _restObj =  _e[0].restangular;
+
+          _restObj.oneUrl('xx', node.restangularUrl).get().then(function (d) {
+            console.log(d);
+            node.restObj = d;
+            ChartService.addNodeToActiveCohortSelection(node);
+          });
+
+        } else {
+          // TODO tell user endpoint is not connected
+        }
+
+
+        // add nodes to active cohort selection
+
+
       });
 
 
