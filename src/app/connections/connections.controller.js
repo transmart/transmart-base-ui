0ccaab9741d493918e104240345997ac9b8ff937
Riza Nugraha <rnugraha@gmail.com>
2016-05-03 17:34:34 +0200

    remove localhost

--- src/app/connections/connections.controller.js
+++ src/app/connections/connections.controller.js
@@ -19,8 +19,8 @@ angular.module('transmartBaseUi')
 
       // Predefined endpoints
       $scope.connections = [
-        {title: 'transmart-gb', url: 'http://transmart-gb.thehyve.net/transmart', isOAuth:  true} ,
-        {title: 'localhost', url: 'http://localhost:8080/transmart', isOAuth:  true}
+        {title: 'transmart-gb', url: 'http://transmart-gb.thehyve.net/transmart', isOAuth:  true},
+        //{title: 'localhost', url: 'http://localhost:8080/transmart', isOAuth:  true}
       ];
 
       $scope.selectedConnection = {};
