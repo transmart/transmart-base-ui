9d397f9722c46f5ee2229522e08ed3257de542b6
Piotr Zakrzewski <piotr@thehyve.nl>
2016-05-31 11:07:08 +0200

    Fix width adjustment issue.

--- src/app/components/cohort-grid/cohort-grid.directive.js
+++ src/app/components/cohort-grid/cohort-grid.directive.js
@@ -66,16 +66,24 @@ angular.module('transmartBaseUi')
           paginationPageSize: 10,
           columnDefs: [],
           data: [],
-          enableFiltering: true
+          enableFiltering: true,
+          onRegisterApi: function (gridApi) {
+            $scope.gridApi = gridApi;
+          }
         };
 
-        $scope.$watch('cohort',function () {
+
+        $scope.$watch('cohort', function () { // I am leaving these two watchers so that we can find out where the
+                                              // issue is with cohorts reset
           console.log('Watcher registered change in cohorts');
         }, true);
 
-        $scope.$watch('headers',function () {
+        $scope.$watch('headers', function () { // I am leaving these two watchers so that we can find out where the
+                                               // issue is with cohorts reset
           console.log('Watcher registered change in headers');
         });// if you set it to true it will cause maximum call stack exceeded exception
+        var WIDTH_PER_COLUMN = 200;
+        $scope.style_modifiers = {'width': WIDTH_PER_COLUMN * 2}; // Starting value never really used
 
         $scope.$on('collectionUpdated', function (event, passedFromBroadcast) {
           console.log('collectionUpdated event fired');
@@ -84,10 +92,18 @@ angular.module('transmartBaseUi')
           var _headers = passedFromBroadcast[1];
           $scope.gridOptions.data.length = 0;       // In order to make sure ui-grid is refreshed also when tables
           $scope.gridOptions.columnDefs.length = 0; // are of the same length
+          $scope.style_modifiers.width = 0; // as above
           $timeout(function () { // this is necessary for ui-grid to notice the change at all
-            $scope.gridOptions.data = _to_table(_cohorts, _headers);
-            $scope.gridOptions.columnDefs = prepareColumnDefs(_headers);
+            $scope.gridOptions.data = _to_table(_cohorts, _headers); // cohorts and hears as they are cannot be displayed
+            $scope.gridOptions.columnDefs = prepareColumnDefs(_headers); // they need to be put in ng-grid format
+            $scope.style_modifiers.width = $scope.gridOptions.columnDefs.length * WIDTH_PER_COLUMN; // cannot set width
+            // as a % of screen - therefore it needs to be set dynamically after each change to number of columns
+            $timeout(function () { // handleWindowResize needs to be called in yet another digest cycle than setting the
+              // data,  columnDefs and the css width attribute
+              $scope.gridApi.core.handleWindowResize();
+            });
           });
+
         });
       }]
     };
