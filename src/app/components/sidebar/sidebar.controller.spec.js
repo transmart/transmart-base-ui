d95c36e435d201b6670ff5ed2fd444992787d1c6
Riza Nugraha <rnugraha@gmail.com>
2016-06-14 12:17:50 +0200

    Add unit testing for sidebar controller

--- src/app/components/sidebar/sidebar.controller.spec.js
+++ src/app/components/sidebar/sidebar.controller.spec.js
@@ -17,13 +17,59 @@ describe('SidebarCtrl', function() {
           });
   }));
 
-  it('is defined', function() {
+  it('is should be defined', function() {
     expect($controller).not.toEqual(undefined);
   });
 
-  it('has no studies loaded', function() {
+  it('should start with no studies loaded', function() {
     expect(scope.publicStudies.length).toEqual(0);
     expect(scope.privateStudies.length).toEqual(0);
   });
 
-});
+  describe('addSearchKey', function () {
+
+    it('should add search key into searchKeys list', function () {
+      scope.searchTerm = 'a';
+      scope.searchKeys = [];
+      scope.addSearchKey();
+      expect(scope.searchKeys.length).toEqual(1);
+    });
+
+    it('should not add search key into searchKeys list if search key is empty', function () {
+      scope.searchTerm = '';
+      scope.searchKeys = [];
+      scope.addSearchKey();
+      expect(scope.searchKeys.length).toEqual(0);
+    });
+
+    it('should not add search key into searchKeys list if search key already exists', function () {
+      scope.searchTerm = 'a';
+      scope.searchKeys = ['a'];
+      scope.addSearchKey();
+      expect(scope.searchKeys.length).toEqual(1);
+    });
+
+  });
+
+  describe('removeAllSearchKeys', function () {
+
+    it('should remove all search keys', function () {
+      scope.searchKeys = ['a', 'b', 'c'];
+      scope.removeAllSearchKeys();
+      expect(scope.searchKeys.length).toEqual(0);
+    });
+
+  });
+
+  describe('removeAllSearchKeys', function () {
+
+    it('should remove all search keys', function () {
+      scope.searchKeys = ['a', 'b', 'c'];
+      scope.removeSearchKey('a');
+      expect(scope.searchKeys.length).toEqual(2);
+    });
+
+  });
+
+
+  });
