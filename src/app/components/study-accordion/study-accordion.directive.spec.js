ec4f482855e0f3c40f1883fca6ea7cdba643f1fd
Riza Nugraha <rnugraha@gmail.com>
2016-06-24 11:17:36 +0200

    Fix study accordion unit-tests

--- src/app/components/study-accordion/study-accordion.directive.spec.js
+++ src/app/components/study-accordion/study-accordion.directive.spec.js
@@ -1,14 +1,15 @@
 'use strict';
 describe('studyAccordion', function() {
-  var scope, template, controller, TreeNodeService;
+  var scope, template, controller, TreeNodeService, $q;
 
   beforeEach(function() {module('transmartBaseUi');});
 
   // load all angular templates (a.k.a html files)
   beforeEach(module('transmartBaseUIHTML'));
 
-  beforeEach(inject(function($compile, $rootScope, $controller, _TreeNodeService_) {
+  beforeEach(inject(function($compile, $rootScope, $controller, _TreeNodeService_, _$q_) {
     scope = $rootScope.$new();
+    $q = _$q_;
     TreeNodeService = _TreeNodeService_;
 
     var element = angular.element("<study-accordion></study-accordion>");
@@ -19,11 +20,13 @@ describe('studyAccordion', function() {
   }));
 
   describe('$scope.populateChilds', function () {
-    var _scope;
+    var _scope, deferred;
 
     beforeEach(function () {
       _scope = scope;
-      spyOn(TreeNodeService, 'getNodeChildren')
+      deferred = $q.defer();
+      spyOn(TreeNodeService, 'setRootNodeAttributes');
+      spyOn(TreeNodeService, 'getNodeChildren').and.returnValue(deferred.promise);
     });
 
     it ('should invoke TreeNodeService.getNodeChildren when populating node children', function () {
@@ -35,39 +38,13 @@ describe('studyAccordion', function() {
         }
       ]};
       _scope.populateChilds(_nodes);
+      expect(TreeNodeService.setRootNodeAttributes).toHaveBeenCalled();
       expect(TreeNodeService.getNodeChildren).toHaveBeenCalled();
     });
 
   });
 
-  describe('$scope.getTree', function () {
-    var _scope;
-
-    beforeEach(function () {
-      _scope = scope;
-      spyOn(TreeNodeService, 'getSingleTree')
-    });
-
-    it ('should invoke TreeNodeService.getSingleTree when expanding a study node', function () {
-      var _study = {
-          open : false
-      };
-      _scope.getTree(_study);
-      expect(TreeNodeService.getSingleTree).toHaveBeenCalledWith(_study);
-      expect(_study.open).toBeTruthy();
-    });
-
-    it ('should change open flag to false when collapsing a study node', function () {
-      var _study = {
-        open : true
-      };
-      _scope.getTree(_study);
-      expect(_study.open).toBeFalsy();
-    });
-  });
-
   describe('$scope.displayMetadata', function () {
-
     var _scope, _nodes = [
       {restObj : {fullName : 'restObj-fullname', metadata : 'restObj-metadata'}, title : 'restObj-title'},
       {_embedded : {ontologyTerm : {name : '_embedded-name', fullName : '_embedded-fullname', metadata : '_embedded-metadata'}}}
