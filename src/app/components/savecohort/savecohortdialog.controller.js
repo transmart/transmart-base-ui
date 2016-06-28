dd652b4c262a19498baaa07da0194bc45365f0d3
Peter Kok <peter@thehyve.nl>
2016-06-28 14:47:39 +0200

    :fish:

--- src/app/components/savecohort/savecohortdialog.controller.js
+++ src/app/components/savecohort/savecohortdialog.controller.js
@@ -20,9 +20,11 @@ angular.module('transmartBaseUi')
         patientSets.customPOST(i2b2Query, undefined, undefined,
             { 'Content-Type': 'text/xml' })
           .then(function(result) {
-            AlertService.add('success', 'Cohort saved OK: "' + $scope.cohortName + '", id: ' + result.id + ', size: ' + result.setSize);
+            AlertService.add('success', 'Cohort saved OK: "' + $scope.cohortName +
+              '", id: ' + result.id + ', size: ' + result.setSize);
           }, function(result) {
-            AlertService.add('danger', 'There was an error saving cohort "' + $scope.cohortName + '": ' + result.data.message);
+            AlertService.add('danger', 'There was an error saving cohort "' +
+              $scope.cohortName + '": ' + result.data.message);
         });
 
         $uibModalInstance.close();
