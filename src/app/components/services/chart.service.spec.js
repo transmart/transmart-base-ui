7f00a88e6b95f5cc6b3c97ab14e07ea5c97392f3
Riza Nugraha <rnugraha@gmail.com>
2016-07-14 16:20:18 +0200

    remove test priority flags

--- src/app/components/services/chart.service.spec.js
+++ src/app/components/services/chart.service.spec.js
@@ -113,17 +113,17 @@ describe('ChartService Unit Tests', function() {
       _label = {ids : 0};
     });
 
-    fit('should remove label from subject labels' , function () {
+    it('should remove label from subject labels' , function () {
       var _res = ChartService.filterSubjectsByLabel(_subjects1, _label);
       expect(_res[0].labels[0]).not.toContain('label1');
     });
 
-    fit('should not remove subject from subject array if subject still has labels' , function () {
+    it('should not remove subject from subject array if subject still has labels' , function () {
       var _res = ChartService.filterSubjectsByLabel(_subjects1, _label);
       expect(_res[0].subject).toEqual(1)
     });
 
-    fit('should  remove subject from subject array if subject still has empty labels' , function () {
+    it('should  remove subject from subject array if subject still has empty labels' , function () {
       var _res = ChartService.filterSubjectsByLabel(_subjects2, _label);
       expect(_res.length).toEqual(0)
     });
