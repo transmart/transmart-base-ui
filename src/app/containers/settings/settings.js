7131a4b315ab271304ad29b67f9e52c23dc4846a
Bernd van der Veen <bernd@thehyve.nl>
2016-07-21 15:23:05 +0200

    TRANSUI-134 Refactor UI-router implementation for state and page definitions

---
+++ src/app/containers/settings/settings.js
@@ -0,0 +1,38 @@
+'use strict';
+
+angular.module('transmartBaseUi')
+    .config(function ($stateProvider) {
+        $stateProvider
+            .state('settings', {
+                parent: 'site',
+                url: '/settings',
+                views: {
+                    '@': {
+                        templateUrl: 'app/containers/settings/settings.html'
+                    },
+                    'navbar@settings': {
+                        templateUrl: 'app/components/navbar/navbar.html',
+                        controller: 'NavbarCtrl'
+//                        controllerAs: 'hnc'
+                    },
+                    'content@settings': {
+                        templateUrl: 'app/containers/settings/settings.html'
+                    },
+                    'footer@settings': {
+                        templateUrl: 'app/components/footer/footer.html',
+                        controller: 'FooterCtrl'
+//                        controllerAs: 'ctf'
+                    }
+                }
+            })
+            .state('settings.connections', {
+                parent: 'settings',
+                url: '/settings/connections',
+                views: {
+                    'content@settings': {
+                        templateUrl: 'app/containers/settings/connections/connections.html',
+                        controller: 'ConnectionsCtrl'
+                    }
+                }
+            });
+    });
