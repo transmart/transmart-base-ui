4461b29d1586b993f47afb65ad6183be528c674e
Bernd van der Veen <bernd@thehyve.nl>
2016-07-22 08:27:39 +0200

    Pull default components nabber and footer up one level and define them in the site state

--- src/app/containers/help/help.js
+++ src/app/containers/help/help.js
@@ -10,18 +10,9 @@ angular.module('transmartBaseUi')
                     '@': {
                         templateUrl: 'app/containers/help/help.html'
                     },
-                    'navbar@help': {
-                        templateUrl: 'app/components/navbar/navbar.html',
-                        controller: 'NavbarCtrl'
-                    },
                     'content@help': {
                         templateUrl: 'app/containers/help/help.content.html'
-                    },
-                    'footer@help': {
-                        templateUrl: 'app/components/footer/footer.html',
-                        controller: 'FooterCtrl'
                     }
-
                 }
             });
     });
