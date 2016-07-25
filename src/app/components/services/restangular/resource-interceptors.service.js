e5f6edd320728f9632bb0745968a764f5cc9a974
Riza Nugraha <rnugraha@gmail.com>
2016-07-25 22:40:06 +0200

    Init restangular interceptors

--- src/app/components/services/restangular/resource-interceptors.service.js
+++ src/app/components/services/restangular/resource-interceptors.service.js
@@ -6,25 +6,23 @@ angular.module('transmartBaseUi')
         var service = {};
 
         service.customResponseInterceptor = function (data, operation, what) {
-
             var _getLastToken = function (what) {
                 var _t = what.split('/').slice(1);
                 return what.indexOf('/') === -1 ? what : _t[_t.length - 1];
             };
 
-            if (operation === 'getList') {
+            if (operation === 'getList' && what !== 'studies') {
                 var _what, resp = data;
                 if (what === 'concepts') {
                     what = 'ontology_terms';
                     resp = data._embedded[what];
-                } else if (what === 'studies') {
-                    // nothing
                 } else {
                     _what = _getLastToken(what);
                     resp = data._embedded[_what];
                 }
                 return resp;
             }
+
             return data;
         };
 
