fe8314e03d0649a30a02363101f218ccfbf3fe29
Riza Nugraha <rnugraha@gmail.com>
2016-07-25 20:05:47 +0200

    Init resource interceptors

--- src/app/components/services/restangular/restangular.service.js
+++ src/app/components/services/restangular/restangular.service.js
@@ -1,12 +1,14 @@
 'use strict';
 
 angular.module('transmartBaseUi')
-    .factory('ResourceService', function (Restangular) {
+    .factory('ResourceService', function (Restangular, ResourceInterceptors) {
 
         var service = {};
 
         service.createResourceServiceByEndpoint = function (endpoint) {
             return Restangular.withConfig(function (RestangularConfigurer) {
+
+                var fnResponseInterceptor = ResourceInterceptors.customResponseInterceptor;
 
                 RestangularConfigurer.setBaseUrl(endpoint.url);
                 RestangularConfigurer.setDefaultHeaders(
@@ -18,35 +20,7 @@ angular.module('transmartBaseUi')
 
                 // Set an interceptor in order to parse the API response
                 // when getting a list of resources
-                RestangularConfigurer.addResponseInterceptor(function (data, operation, what) {
-
-                    /**
-                     * Get the last token when requested model is a string path
-                     * @param what
-                     * @returns {*}
-                     * @private
-                     */
-                    var _getLastToken = function (what) {
-                        var _t = what.split('/').slice(1);
-                        return what.indexOf('/') === -1 ? what : _t[_t.length - 1];
-                    };
-
-                    if (operation === 'getList') {
-                        var _what, resp = data;
-                        if (what === 'concepts') {
-                            what = 'ontology_terms';
-                            resp = data._embedded[what];
-                        } else if (what === 'studies') {
-                            // nothing
-                        } else
-                        {
-                            _what = _getLastToken(what);
-                            resp = data._embedded[_what];
-                        }
-                        return resp;
-                    }
-                    return data;
-                });
+                RestangularConfigurer.addResponseInterceptor(fnResponseInterceptor);
 
                 // Using self link for self reference resources
                 RestangularConfigurer.setRestangularFields({
