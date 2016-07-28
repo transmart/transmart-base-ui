f758c500e3217625306e4b85a01bb8d5c0cc65d4
Bo Gao <bo.gao111@gmail.com>
2016-07-28 16:06:15 +0200

    using shell

--- gulp/docs.js
+++ gulp/docs.js
@@ -2,36 +2,13 @@
 
 var gulp = require('gulp');
 var shell = require('gulp-shell');
-var isThere = require("is-there");
-var mkdirp = require('mkdirp');
-var gutil = require('gulp-util');
 
-gulp.task('prepareDocDirectories', function () {
-    var docsDir = 'dist/docs';
-    var tutorialsDir = docsDir + '/tutorials';
-
-    if(!isThere(docsDir)) {
-        mkdirp(docsDir, function (err) {
-            if (err) console.error(err)
-            else gutil.log('Create directory ' + docsDir + '...');
-        });
-    }
-
-    if(!isThere(tutorialsDir)) {
-        mkdirp(tutorialsDir, function (err) {
-            if (err) console.error(err)
-            else gutil.log('Create directory ' + tutorialsDir + '...');
-        });
-    }
-
-});
-
-gulp.task('docs', ['prepareDocDirectories'], shell.task([
-    'node_modules/jsdoc/jsdoc.js '+
-    '-c node_modules/angular-jsdoc/common/conf.json '+   // config file
-    '-t node_modules/angular-jsdoc/angular-template '+   // template file
-    '-d dist/docs '+                          // output directory
+gulp.task('docs', shell.task([
+    'mkdir "dist/docs" \\' +
+    '&& node_modules/jsdoc/jsdoc.js ' +
+    '-c node_modules/angular-jsdoc/common/conf.json ' +   // config file
+    '-t node_modules/angular-jsdoc/angular-template ' +   // template file
+    '-d dist/docs ' +                          // output directory
     './README.md ' +                          // to include README.md as index contents
-    '-r src/app/ ' +                 // source code directory
-    '-u dist/docs/tutorials'   // tutorials directory
+    '-r src/app/ '                 // source code directory
 ]));
