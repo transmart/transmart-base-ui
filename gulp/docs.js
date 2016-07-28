89c51c377d314694f26a9c0f2ed3e9904afcd8cd
Bo Gao <bo.gao111@gmail.com>
2016-07-28 16:29:20 +0200

    update docs

--- gulp/docs.js
+++ gulp/docs.js
@@ -4,8 +4,7 @@ var gulp = require('gulp');
 var shell = require('gulp-shell');
 
 gulp.task('docs', shell.task([
-    'mkdir "dist/docs" \\' +
-    '&& node_modules/jsdoc/jsdoc.js ' +
+    'node_modules/jsdoc/jsdoc.js ' +
     '-c node_modules/angular-jsdoc/common/conf.json ' +   // config file
     '-t node_modules/angular-jsdoc/angular-template ' +   // template file
     '-d dist/docs ' +                          // output directory
