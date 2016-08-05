b206cc0e58751e905cf0033e02410e13b314c717
Bernd van der Veen <bernd@thehyve.nl>
2016-08-05 11:11:37 +0200

    Tree Node mocks

--- src/app/components/services/tree-node/tree-node.mock.js
+++ src/app/components/services/tree-node/tree-node.mock.js
@@ -4,185 +4,108 @@
  * @Description Service layer exposing a mock layer for QueryBuilder operations.
  */
 angular.module('transmartBaseUi')
-    .factory('QueryBuilderMocks', ['$q',
-        function ($q) {
+    .factory('TreeNodeMocks', ['ResourceService',
+        function (ResourceService) {
             var mock = {};
+            var _restangular = ResourceService.createResourceServiceByEndpoint({});
 
-            mock.filterCohortFullStudy = function() {
-                return [{
-                    name: 'Organism',
-                        type: 'string',
-                    label: '\\Public Studies\\GSE8581\\Subjects\\Organism\\',
-                    study: {
-						id: 'GSE8581',
-							type: 'public',
-							_embedded: {
-							ontologyTerm: {
-								fullName: '\\Public Studies\\GSE8581\\'
-							}
-						}
-                    },
-                    filters: []
-                }]
-            };
-
-            mock.filterCohortFullStudyResponse = function() {
-                return [{
-                    panel_number: 1,
-                    invert: 0,
-                    total_item_occurrences: 1,
-                    item: {
-                        item_name: "GSE8581",
-                        item_key: "\\\\Public Studies\\Public Studies\\GSE8581\\",
-                        tooltip: "\\Public Studies\\GSE8581\\",
-                        class: "ENC"
-                    }
-                }];
-            };
-
-            mock.cohortFiltersWithCategories = function() {
-                return [
-                    {
-                        name: 'Organism',
-                        type: 'string',
-                        label: '\\Public Studies\\GSE8581\\Subjects\\Organism\\',
-                        study: {
-                            type: 'public'
-                        },
-                        filters: []
-                    },
-                    {
-                        name: 'Sex',
-                        type: 'string',
-                        label: '\\Public Studies\\GSE8581\\Subjects\\Sex\\',
-                        study: {
-                            type: 'public'
-                        },
-                        filters: ['female']
-                    },
-                    {
-                        name: 'Diagnosis',
-                        type: 'string',
-                        label: '\\Public Studies\\GSE8581\\Subjects\\Diagnosis\\',
-                        study: {
-                            type: 'public'
-                        },
-                        filters: ['carcinoid', 'hematoma']
-                    }
-                ];
-            };
-
-            mock.cohortFiltersWithCategoriesResponse = function() {
-                return [{
-                    panel_number: 1,
-                    invert: 0,
-                    total_item_occurrences: 1,
-                    item: [{
-                        item_name: "female",
-                        item_key: "\\\\Public Studies\\Public Studies\\GSE8581\\Subjects\\Sex\\female",
-                        tooltip: "\\Public Studies\\GSE8581\\Subjects\\Sex\\female",
-                        class: "ENC"
-                    }]
-                }, {
-                    panel_number: 2,
-                    invert: 0,
-                    total_item_occurrences: 1,
-                    item: [{
-                        item_name: "carcinoid",
-                        item_key: "\\\\Public Studies\\Public Studies\\GSE8581\\Subjects\\Diagnosis\\carcinoid",
-                        tooltip: "\\Public Studies\\GSE8581\\Subjects\\Diagnosis\\carcinoid",
-                        class: "ENC"
-                    }, {
-                        item_name: "hematoma",
-                        item_key: "\\\\Public Studies\\Public Studies\\GSE8581\\Subjects\\Diagnosis\\hematoma",
-                        tooltip: "\\Public Studies\\GSE8581\\Subjects\\Diagnosis\\hematoma",
-                        class: "ENC"
-                    }]
-                }];
-            };
-
-            mock.cohortFiltersWithNumberRanges = function() {
-                return [
-                    {
-                        name: 'Age',
-                        type: 'number',
-                        label: '\\Public Studies\\GSE8581\\Subjects\\Age\\',
-                        study: {
-                            type: 'public'
-                        },
-                        filters: [[65, 70]]
-                    },
-                    {
-                        name: 'Height',
-                        type: 'number',
-                        label: '\\Public Studies\\GSE8581\\Subjects\\Height\\',
-                        study: {
-                            type: 'public'
-                        },
-                        filters: []
-                    }
-                ];
-            };
-
-            mock.cohortFiltersWithNumberRangesResponse = function() {
-                return [{
-                    panel_number: 1,
-                    invert: 0,
-                    total_item_occurrences: 1,
-                    item: [{
-                        item_name: 'Age',
-                        item_key: '\\\\Public Studies\\Public Studies\\GSE8581\\Subjects\\Age\\',
-                        tooltip: '\\Public Studies\\GSE8581\\Subjects\\Age\\',
-                        class: 'ENC',
-                        constrain_by_value: {
-                            value_operator: 'BETWEEN',
-                            value_constraint: '65 and 70',
-                            value_type: 'NUMBER'
-                        }
-                    }]
-                }];
-            };
-
-            mock.cohortFiltersToXML = function() {
+            mock.treenodeResponse = function() {
                 return {
-                    cohortName: 'my cohort',
-                    cohortFilters: [
-                        {
-                            name: 'Age',
-                            type: 'number',
-                            label: '\\Public Studies\\GSE8581\\Subjects\\Age\\',
-                            study: {
-                                type: 'public'
-                            },
-                            filters: [[65, 70]]
+                    "name": "Endpoints",
+                    "key": "\\\\Public Studies\\Public Studies\\GSE8581\\Endpoints\\",
+                    "fullName": "\\Public Studies\\GSE8581\\Endpoints\\",
+                    "type": "CATEGORICAL_OPTION",
+                    "_links": {
+                        "self": {
+                            "href": "/studies/gse8581/concepts/Endpoints"
                         },
-                        {
-                            name: 'Diagnosis',
-                            type: 'string',
-                            label: '\\Public Studies\\GSE8581\\Subjects\\Diagnosis\\',
-                            study: {
-                                type: 'public'
+                        "observations": {
+                            "href": "/studies/gse8581/concepts/Endpoints/observations"
+                        },
+                        "children": [
+                            {
+                                "href": "/studies/gse8581/concepts/Endpoints/Diagnosis",
+                                "title": "Diagnosis"
                             },
-                            filters: ['carcinoid', 'hematoma']
+                            {
+                                "href": "/studies/gse8581/concepts/Endpoints/FEV1",
+                                "title": "FEV1"
+                            },
+                            {
+                                "href": "/studies/gse8581/concepts/Endpoints/Forced%20Expiratory%20Volume%20Ratio",
+                                "title": "Forced Expiratory Volume Ratio"
+                            }
+                        ],
+                        "parent": {
+                            "href": "/studies/gse8581/concepts/ROOT"
                         }
-                    ]
+                    }
                 }
             };
 
-            mock.cohortFiltersToXMLResponse = function() {
-                var q = '<query_definition><query_name>my cohort</query_name><panel>' +
-                    '<panel_number>1</panel_number><invert>0</invert><total_item_occurrences>1</total_item_occurrences>' +
-                    '<item><item_name>Age</item_name><item_key>\\\\Public Studies\\Public Studies\\GSE8581\\Subjects\\Age\\</item_key>' +
-                    '<tooltip>\\Public Studies\\GSE8581\\Subjects\\Age\\</tooltip><class>ENC</class>' +
-                    '<constrain_by_value><value_operator>BETWEEN</value_operator><value_constraint>65 and 70</value_constraint' +
-                    '><value_type>NUMBER</value_type></constrain_by_value></item></panel><panel><panel_number>2</panel_number' +
-                    '><invert>0</invert><total_item_occurrences>1</total_item_occurrences><item><item_name>carcinoid</item_name>' +
-                    '<item_key>\\\\Public Studies\\Public Studies\\GSE8581\\Subjects\\Diagnosis\\carcinoid</item_key>' +
-                    '<tooltip>\\Public Studies\\GSE8581\\Subjects\\Diagnosis\\carcinoid</tooltip><class>ENC</class></item>' +
-                    '<item><item_name>hematoma</item_name><item_key>\\\\Public Studies\\Public Studies\\GSE8581\\Subjects\\Diagnosis\\hematoma</item_key>' +
-                    '<tooltip>\\Public Studies\\GSE8581\\Subjects\\Diagnosis\\hematoma</tooltip><class>ENC</class></item></panel></query_definition>'
+            mock.subjects = function() {
+                return {
+                    "_links": {
+                        "self": {
+                            "href": "/studies/av_app_demo/subjects"
+                        }
+                    },
+                    "_embedded": {
+                        "subjects": [
+                            {
+                                "religion": null,
+                                "maritalStatus": null,
+                                "race": null,
+                                "id": 1000430083,
+                                "birthDate": null,
+                                "age": null,
+                                "deathDate": null,
+                                "trial": "AV_APP_DEMO",
+                                "inTrialId": "LN1",
+                                "sex": "UNKOWN",
+                                "_links": {
+                                    "self": {
+                                        "href": "/studies/av_app_demo/subjects/1000430083"
+                                    }
+                                }
+                            },
+                            {
+                                "religion": null,
+                                "maritalStatus": null,
+                                "race": null,
+                                "id": 1000430082,
+                                "birthDate": null,
+                                "age": null,
+                                "deathDate": null,
+                                "trial": "AV_APP_DEMO",
+                                "inTrialId": "LN2",
+                                "sex": "UNKOWN",
+                                "_links": {
+                                    "self": {
+                                        "href": "/studies/av_app_demo/subjects/1000430082"
+                                    }
+                                }
+                            }
+                        ]
+                    }
+                };
+            };
 
-                return q;
+            mock.dummyNode = function() {
+                return {
+                    restObj: _restangular,
+                    _links: {
+                        children: []
+                    },
+                    _embedded: {
+                        ontologyTerm: {
+                            _links: {
+                                children: []
+                            }
+                        }
+                    },
+                    nodes: []
+                };
             };
 
             return mock;
