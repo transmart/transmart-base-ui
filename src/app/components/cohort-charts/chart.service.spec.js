397488b0d948749dcc7b2e253bbd1663e1e0ee4d
Bo Gao <bo.gao111@gmail.com>
2016-08-30 17:56:02 +0200

    change fdescribe back to describe

--- src/app/components/cohort-charts/chart.service.spec.js
+++ src/app/components/cohort-charts/chart.service.spec.js
@@ -279,7 +279,7 @@ describe('ChartService Unit Tests', function () {
 
     });
 
-    fdescribe('createCohortChart', function () {
+    describe('createCohortChart', function () {
         var label, el, subjects;
 
         beforeEach(function () {
